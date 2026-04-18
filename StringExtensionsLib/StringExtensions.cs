8cea1d2ef54466c27f613404ea054d914e2b2edd
Deksan <deksansaid5@gmail.com>
2026-04-18 15:52:30 -0400

    Add StringExtensions class library

---
+++ StringExtensionsLib/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace StringExtensionLib
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpperCase(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return false;
+            return char.IsUpper(str[0]);
+        }
+    }
+}
